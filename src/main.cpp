d132a3b8cf7d3c4a4420db5995c2b922deef4d5e
AdBol05 <adam.b@volny.cz>
2023-11-04 19:29:40 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,13 @@ void saveToCSV(const String deviceType, const String deviceName, const String de
 
 void setup() {
   Serial.begin(9600);
-  
+
   Serial.println("Starting BLE...");
   if (!BLE.begin()) {Serial.println("Starting BLE failed!"); while (1);}
-  
+
   Serial.println("Initializing SD card...");
   if (!SD.begin(4)) {Serial.println("SD card initialization failed!"); while (1);}
-  
+
   dataFile = SD.open("scanned_devices.csv", FILE_WRITE);
   if (dataFile) {dataFile.println("Type,Name,Address"); dataFile.close();}
   else {Serial.println("Error opening data file");}
@@ -40,7 +40,7 @@ void loop() {
   BLE.scan();
   BLEDevice peripheral = BLE.available();
   BLEDevice central = BLE.central();
-  
+
   if (peripheral) {
     Serial.println("Fount peripheral device");
     Serial.print(" > name: ");
@@ -48,10 +48,10 @@ void loop() {
     Serial.print(" > adress: ");
     Serial.println(peripheral.address());
     Serial.println();
-    
+
     saveToCSV("peripheral", peripheral.localName(), peripheral.address());
   }
-  
+
   if (central) {
     Serial.println("Fount central device");
     Serial.print(" > name: ");
@@ -59,10 +59,10 @@ void loop() {
     Serial.print(" > address: ");
     Serial.println(central.address());
     Serial.println();
-    
+
     saveToCSV("central", central.localName(), central.address());
   }
-  
+
   delay(10000);
   //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 }
